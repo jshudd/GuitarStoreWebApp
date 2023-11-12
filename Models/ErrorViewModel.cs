c6da057d52cebe65c26b35af00fd79aae0341ec1
Jeremy <jshudd@gmail.com>
2023-11-11 18:02:42 -0600

    created new project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GuitarStoreWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
