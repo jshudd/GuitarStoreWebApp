4434ca1a7a78a3d15d47ee7f104497974e8902e5
Jeremy <jshudd@gmail.com>
2023-11-11 18:14:34 -0600

    added properties for model

--- Models/ElectricGuitars.cs
+++ Models/ElectricGuitars.cs
@@ -6,6 +6,20 @@ namespace GuitarStoreWebApp.Models
 		public ElectricGuitars()
 		{
 		}
-	}
-}
 
+		public int GuitarID { get; set; }
+		public int BrandID { get; set; }
+		public string Model { get; set; }
+		public double Price { get; set; }
+		public string Color { get; set; }
+		public int StringNum { get; set; }
+		public int PickupNum { get; set; }
+		public int FretNum { get; set; }
+		public int BodyStyle { get; set; }
+		public int PickupBridgeType { get; set; }
+        public int PickupMiddleType { get; set; }
+        public int PickupNeckType { get; set; }
+		public int PickupExtra { get; set; }
+		public byte[] Image { get; set; }
+    }
+}
