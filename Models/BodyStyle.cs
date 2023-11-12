1b6cae784814a4f1344f16d7f5443ae351c2b1e0
Jeremy <jshudd@gmail.com>
2023-11-11 18:21:40 -0600

    created BodyStyle class

---
+++ Models/BodyStyle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace GuitarStoreWebApp.Models
+{
+	public class BodyStyle
+	{
+		public BodyStyle()
+		{
+		}
+
+		public int BodystyleID { get; set; }
+		public string BodyType { get; set; }
+	}
+}
+
