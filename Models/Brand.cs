a3b2bd1db4a506207caf5f4c91ab6f46d2ac27e9
Jeremy <jshudd@gmail.com>
2023-11-11 18:18:37 -0600

    created Brand model

---
+++ Models/Brand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace GuitarStoreWebApp.Models
+{
+	public class Brand
+	{
+		public Brand()
+		{
+		}
+
+		public int BrandID { get; set; }
+		public string Name { get; set; }
+	}
+}
+
