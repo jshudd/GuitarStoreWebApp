bcb4a61a6d8ada5844d18b983d5e7a871543205f
Jeremy <jshudd@gmail.com>
2023-11-11 18:20:03 -0600

    created PickupType class

---
+++ Models/Pickups.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace GuitarStoreWebApp.Models
+{
+	public class Pickups
+	{
+		public Pickups()
+		{
+		}
+
+		public int PickupID { get; set; }
+		public string PickupType { get; set; }
+	}
+}
+
